3ddea48586fef9548a095fa28aae654a1343b499
Anna Bramarova <annabramarova@gmail.com>
2024-10-18 16:19:15 +0300

    mob menu

--- src/partials/header/mobileMenu.js
+++ src/partials/header/mobileMenu.js
@@ -1,37 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const getElement = selector => document.querySelector(selector);
+  const burgerBtn = document.querySelector('[data-burger-btn]');
+  const burgerIcon = document.querySelector('[data-burger-icon]');
+  const menu = document.querySelector('[data-menu]');
 
-  const toggleClass = (element, className) =>
-    element.classList.toggle(className);
+  burgerBtn.addEventListener('click', () => {
+    const isOpen = menu.getAttribute('data-menu-open') === 'true';
+    menu.setAttribute('data-menu-open', !isOpen);
+    burgerIcon.setAttribute('data-menu-open', !isOpen);
+  });
 
-  const initMobileMenu = () => {
-    const burgerBtn = getElement('.js-burger-btn');
-    const mobileMenu = getElement('.js-mobile-menu');
-    const burgerIcon = getElement('.js-burger-icon');
-    const body = document.body;
-
-    const handleMenuToggle = () => {
-      toggleClass(mobileMenu, 'menu-open');
-      toggleClass(burgerIcon, 'burger-active');
-      toggleClass(body, 'is-menu-open');
-
-      setTimeout(() => toggleClass(mobileMenu, 'menu-bg'), 10);
-    };
-
-    const handleOutsideClick = event => {
-      if (
-        !mobileMenu.contains(event.target) &&
-        !burgerBtn.contains(event.target)
-      ) {
-        mobileMenu.classList.remove('menu-open', 'menu-bg');
-        burgerIcon.classList.remove('burger-active');
-        body.classList.remove('is-menu-open');
-      }
-    };
-
-    burgerBtn.addEventListener('click', handleMenuToggle);
-    document.addEventListener('click', handleOutsideClick);
-  };
-
-  initMobileMenu();
+  document.addEventListener('click', event => {
+    if (!menu.contains(event.target) && !burgerBtn.contains(event.target)) {
+      menu.setAttribute('data-menu-open', 'false');
+      burgerIcon.setAttribute('data-menu-open', 'false');
+    }
+  });
 });
