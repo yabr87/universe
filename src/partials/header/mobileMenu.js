8916bbdca425be424f6d2eb8749ef6d32c64cc3f
Anna Bramarova <annabramarova@gmail.com>
2024-10-19 15:23:45 +0300

    header fin
    зафіксувала хедер та додала зникання при прокрутці

--- src/partials/header/mobileMenu.js
+++ src/partials/header/mobileMenu.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('.header');
   const burgerWrapper = document.querySelector('[data-burger-btn]');
   const desktopMenu = document.querySelector('[data-desktop-menu]');
   const mobileMenu = document.querySelector('[data-mob-menu]');
+
+  let lastScrollTop = 0;
+  let isHeaderVisible = true;
 
   burgerWrapper.addEventListener('click', () => {
     burgerWrapper.querySelector('[data-burger-icon]').classList.toggle('open');
@@ -20,6 +24,18 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop && isHeaderVisible) {
+      header.classList.add('hidden');
+      isHeaderVisible = false;
+    } else if (scrollTop < lastScrollTop && !isHeaderVisible) {
+      header.classList.remove('hidden');
+      isHeaderVisible = true;
+    }
+
+    lastScrollTop = scrollTop;
+
     if (desktopMenu.classList.contains('open')) {
       closeMenus();
     }
